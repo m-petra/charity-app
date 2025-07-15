63a9e5fbc9e350cf93070fa087dae092836bab8c
Petra Mezei <mezeipetra0410@gmail.com>
2025-07-15 15:29:50 +0200

    fix package issues

--- apps/charity-app-web/src/app/app.config.server.ts
+++ apps/charity-app-web/src/app/app.config.server.ts
@@ -1,10 +1,11 @@
-import { mergeApplicationConfig, ApplicationConfig } from "@angular/core";
-import { provideServerRendering, withRoutes } from "@angular/ssr";
-import { appConfig } from "./app.config";
-import { serverRoutes } from "./app.routes.server";
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRouting } from '@angular/ssr';
+import { appConfig } from './app.config';
+import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes))],
+  providers: [provideServerRendering(), provideServerRouting(serverRoutes)],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
